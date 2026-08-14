4c1610355d5fbb23943efb41aa70d1f7dfb005dd
Yan Campêlo das Chagas <yanchagas2004@gmail.com>
2026-08-14 20:39:54 -0300

    fix: Notebooks de report e testes de ordem

--- olist/notebooks/orders_reports.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import data_profiling
-from data_profiling import ProfileReport
-
-df = pd.read_csv("data/raw/olist_orders_dataset.csv")
-
-profile = ProfileReport(df, title="YData Profiling Report", explorative=True)
-
-profile.to_file("reports/orders_report.html")
